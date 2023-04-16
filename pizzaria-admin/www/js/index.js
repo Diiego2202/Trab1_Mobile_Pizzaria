eb094f8c945a298f33e4a977f90365d0e1966b38
Diego Calciolari Gordiano <diego.gordiano@acad.espm.br>
2023-04-16 20:29:36 -0300

    funções foto, salvar, excluir e cancelar funcionais

--- pizzaria-admin/www/js/index.js
+++ pizzaria-admin/www/js/index.js
@@ -1,25 +1,24 @@
 var pizza;
 var preco;
 var imagem;
-var PIZZARIA_ID = 'Pizzaria_Diloko'
-
-var itensCardapio;
-var itemAtual = 0;
+var PIZZARIA_ID = 'Pizzaria_Diloko';
+var listaPizzasCadastradas = [];
 
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    //document.getElementById('deviceready').classList.add('ready');
     cordova.plugin.http.setDataSerializer('json');
+
     imagem = document.getElementById('imagem');
     pizza = document.getElementById('pizza');
     preco = document.getElementById('preco');
+    document.getElementById('btnCancelar').addEventListener('click', cancelar);
     document.getElementById('btnNovo').addEventListener('click', novo);
-    document.getElementById('btnFoto').addEventListener('click', foto);
     document.getElementById('btnSalvar').addEventListener('click', salvar);
     document.getElementById('btnExcluir').addEventListener('click', excluir);
-    document.getElementById('btnCancelar').addEventListener('click', cancelar);
+    document.getElementById('btnFoto').addEventListener('click', foto);
+    carregarPizzas();
 }
 
 function novo() {
@@ -27,8 +26,7 @@ function novo() {
     appcadastro.style.display = 'flex'; // exibe cadastro
 }
 
-//ta dando erro
-const foto = () => {
+function foto() {
     navigator.camera.getPicture(onSuccess, 
                                 onFail, 
                                 { quality: 50, 
@@ -36,7 +34,7 @@ const foto = () => {
                                );  
     
     function onSuccess(imageData) {
-        preview.style.backgroundImage = "url('data:image/jpeg;base64," + imageData + "')"; 
+        imagem.style.backgroundImage = "url('data:image/jpeg;base64," + imageData + "')"; 
     }  
     
     function onFail(message) { 
@@ -45,7 +43,6 @@ const foto = () => {
 }
 
 function salvar() {
-    // espeficica o formato JSON para os dados enviados
     cordova.plugin.http.setDataSerializer('json');
     cordova.plugin.http.post('https://pedidos-pizzaria.glitch.me/admin/pizza/', {
         pizzaria: PIZZARIA_ID, 
@@ -53,35 +50,72 @@ function salvar() {
         preco: preco.value, 
         imagem: imagem.style.backgroundImage
     }, {}, function(response) {
-        // verifica se deu certo (status = 200)
         alert(response.status);
     }, function(response) {
         alert(response.error);
     });
+    carregarPizzas();
 }
 
 function excluir() {
-    //terminar função de deletar
-    cordova.plugin.http.delete('https://pedidos-pizzaria.glitch.me/admin/pizzas/:PIZZARIA_ID', {}, {}, 
-    function(response) {
-        itensCardapio = JSON.parse(response.data);
-        atualizarTela();
+    var NOME_PIZZA = document.getElementById('pizza').value;
+    cordova.plugin.http.delete('https://pedidos-pizzaria.glitch.me/admin/pizza/'+ PIZZARIA_ID + '/' +  NOME_PIZZA,{ 
+    }, {}, function(response) {
+        alert(response.status);
     }, function(response) {
         alert(response.error);
     });
+    carregarPizzas();
 }
 
 function cancelar() {  
-    //voltar pra tela inicial
+    applista.style.display = 'flex'; // exibe lista
+    appcadastro.style.display = 'none'; // oculta cadastro
 }
 
 function carregarPizzas() {
-    //terminar função de mostrar as pizzas cadastradas
-    cordova.plugin.http.get('https://pedidos-pizzaria.glitch.me/admin/pizzas/:PIZZARIA_ID', {}, {}, 
-    function(response) {
-        itensCardapio = JSON.parse(response.data);
-        atualizarTela();
+    cordova.plugin.http.get('https://pedidos-pizzaria.glitch.me/admin/pizzas/' + PIZZARIA_ID, {   
+    }, {}, function(response) {
+        if(response.data != null) {
+            listaPizzasCadastradas = JSON.parse(response.data);
+            listaPizzasCadastradas.forEach((item, idx) => {
+                const novo = document.createElement('div');
+                novo.classList.add('linha');
+                novo.innerHTML = item.pizza;
+                novo.id = idx;
+                novo.onclick = function () {
+                    carregarDadosPizza(novo.id);
+                    applista.style.display = 'none'; // oculta lista
+                    appcadastro.style.display = 'flex'; // exibe cadastro
+                };
+                listaPizzas.appendChild(novo);
+            });
+        } alert(response.status);
     }, function(response) {
         alert(response.error);
-    });
+    });     
+}
+
+function carregarDadosPizza(id) {
+    imagem.style.backgroundImage = listaPizzasCadastradas[id].imagem;
+    pizza.value = listaPizzasCadastradas[id].pizza;
+    preco.value = listaPizzasCadastradas[id].preco;
+    let idPizza = listaPizzasCadastradas[id]._id;
+    btnSalvar.onclick = function() {
+        attPizza(idPizza);
+    }
+}
+
+function attPizza(_id_pizza) {
+    cordova.plugin.http.put('https://pedidos-pizzaria.glitch.me/admin/pizza/', {
+        pizzaId: _id_pizza,
+        pizzaria: PIZZARIA_ID,
+        pizza: pizza.value,
+        preco: preco.value,
+        imagem: imagem.style.backgroundImage
+    }, {}, function(response){
+        alert(response.status);
+    }, function(response){
+        alert(response.error);
+    })
 }
